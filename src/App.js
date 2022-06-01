e20a12352b84a042356a0c4832e00f03230d88f2
Jason Wisniewski <87104768+JasonWisniewski@users.noreply.github.com>
2022-06-01 09:45:45 -0600

    adding buttons and functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {IonButton, IonToast} from '@ionic/react';
 
 function App() {
+
+    const [showToast, setShowToast] = useState(false);
+    const handleClick = () => {
+        setShowToast(true);
+        setTimeout(() => setShowToast(false), 1500);
+    };
+
   return (
     <div>
       <header>
         <h1>My App</h1>
       </header>
       <main>
-        <p>Add some content here…</p>
+          <IonButton onClick={handleClick}> click me bitch </IonButton>
+          <IonToast isOpen={showToast} message={'hello world!'} />
+          {}
       </main>
     </div>
   );
